9a6891cb8c04d49717550cf0ca377c486629684c
nuts-rice <red44443@gmail.com>
2023-12-13 20:07:50 -0500

    💄 source window

---
+++ crabbooru/src/client/src/viewer/sources_window.rs
@@ -0,0 +1,86 @@
+pub use crate::api::Api;
+pub use crate::api::{Page, Site};
+use std::collections::HashMap;
+use winit::raw_window_handle::HasRawWindowHandle;
+use winit::window::{Window, WindowBuilder, WindowButtons, WindowId};
+use winit::{event::Event, event::WindowEvent, event_loop::EventLoop};
+
+type SourceUrl = String;
+pub struct SourcesWindow {
+    selected: Vec<Site>,
+    parent: WindowId,
+    windows: HashMap<WindowId, Window>,
+}
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+pub struct SourcesRow {
+    site: Site,
+    //todo: tie these to ui elements
+    checkbox: bool,
+    labels: Vec<String>,
+    button: WindowButtons,
+}
+
+impl SourcesWindow {
+    pub fn new() -> Self {
+        Self {
+            selected: Vec::new(),
+            parent: WindowId::from(0),
+            windows: HashMap::new(),
+        }
+    }
+    pub fn sources_window(&mut self, selected: Vec<Site>, parent: &mut WindowId) -> Result<()> {
+        let event_loop = EventLoop::new().unwrap();
+        let mut builder = WindowBuilder::new()
+            .with_title("Sources")
+            .with_inner_size(winit::dpi::LogicalSize::new(400, 200));
+        let window = builder.build(&event_loop).unwrap();
+
+        event_loop.run(move |event: Event<()>, elwt| {
+            if let Event::WindowEvent { window_id, event } = event {
+                match event {
+                    WindowEvent::CloseRequested => {
+                        println!("The close button was pressed; stopping");
+                        self.windows.clear();
+                        elwt.exit();
+                    }
+                    WindowEvent::RedrawRequested => {
+                        if let Some(window) = self.windows.get(&window_id) {
+                            println!("Redrawing window {:?}", window.id());
+                        }
+                    }
+                    // WindowEvent::MouseInput => {
+                    //     todo!() }
+                    _ => (),
+                }
+            }
+        });
+
+        todo!()
+    }
+    pub fn add_source(&mut self, url: SourceUrl) {
+        todo!()
+    }
+    pub fn is_source_valid(&self, url: SourceUrl) -> bool {
+        todo!()
+    }
+    pub fn check_all_sources(&self) {
+        todo!()
+    }
+    pub fn prepare_site_settings(&self) {
+        todo!()
+    }
+    pub fn delete_site(&self) {
+        todo!()
+    }
+
+    pub fn check_updates(&self) {
+        let mut is_checked: bool = false;
+        let mut is_unchecked: bool = false;
+        for row in self.selected.iter() {
+            // if row.is_checked() {
+            //     row.;
+            // }
+            // println!("Page: {:?}", page);
+        }
+    }
+}
